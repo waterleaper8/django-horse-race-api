2757123ad9491a5b3b351ce1ca66d0db3a43965b
waterleaper <waterleaper8@gmail.com>
2021-11-17 16:24:57 +0900

    Hide secret key

--- horse_race_api/settings.py
+++ horse_race_api/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gi*p$t#w0-zpzp$)9-kpjuz!1fd25&(l^mvk3)ep!-$xjfa2uu'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
