b54c4d932a338ac67d04e12cdbca20a1187f7c47
waterleaper@sakura <waterleaper8@gmail.com>
2023-08-08 23:25:19 +0900

    fix-requirements

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 while true
 do
         source /home/waterleaper/.profile
-        source /home/waterleaper/.bashrc
         cd /home/waterleaper/horse-race-api
         source .venv/bin/activate
         python manage.py runserver
