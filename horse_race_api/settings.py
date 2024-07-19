bcbb4020459977dbe0739884477c076d1a1170df
waterleaper <waterleaper8@gmail.com>
2024-07-19 16:23:21 +0900

    update

--- horse_race_api/settings.py
+++ horse_race_api/settings.py
@@ -59,7 +59,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://127.0.0.1:5500",
     'http://localhost:3000',
     'https://waterleaper.net',
-    'https://horse-api.waterleaper.net'
+    'https://horse-api.waterleaper.net',
+    'https://os3-369-17552.vs.sakura.ne.jp'
 ]
 
 ROOT_URLCONF = 'horse_race_api.urls'
