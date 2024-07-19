b1d481598fc899ba17d662f4d43e963a05ec4aa4
waterleaper <waterleaper8@gmail.com>
2024-07-19 15:49:04 +0900

    update

--- horse_race_api/urls.py
+++ horse_race_api/urls.py
@@ -2,15 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('api/horse/admin/', admin.site.urls),
-    path('api/horse/prediction/',
-        include('prediction.urls')),
-    path('api/horse/odds/',
-        include('odds.urls')),
-=======
     path('admin/', admin.site.urls),
     path('prediction/', include('prediction.urls')),
     path('odds/', include('odds.urls')),
->>>>>>> a063277 (update)
 ]
